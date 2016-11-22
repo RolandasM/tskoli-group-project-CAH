b8d57888c16d9b2c46cddae7f2170a14f7c194d3
JonBjSig <Jaunanb@gmail.com>
2016-11-22 16:10:55 +0000

    test testest testicles
    swag

---
+++ main.py
@@ -0,0 +1,8 @@
+#main running file for Cards Against Humanity pycharm game
+
+Playing = True#set to false to stop game
+UserAnswering = True#value determining if the user is the one answering questions or questioning
+CardsInHand = [](str)#array/list holding the cards the user has in hand
+
+while Playing:
+    a = 1
