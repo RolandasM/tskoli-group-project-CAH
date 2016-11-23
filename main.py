7e95729815dc8fd1c7c865c42049feafd54ec8b7
JonBjSig <Jaunanb@gmail.com>
2016-11-23 16:57:02 +0000

    A few more touchups to the groundwork
    Added a loop that runs every frame and checks what screen elements need
    to be drawn and calls the appropriate functions with the appropriate
    values.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ gameState = "" #identifies current game state:
 #gameend = the game has ended, scores are tallied
 #quit = user has chosen to quit the game
 
-drawArr = [](str)#
+drawArr = [](str)#array of what parts of the game need to be drawn on screen
 roundPos = 0#the current round position in the game, from 0 to however many players are in the game
 bigBlackDeck = [](card)#the deck holding the black cards. the deck is really big and really black
 players = [](player)#array of players in the user's game, used for drawing the game. to be populated by the server
@@ -22,12 +22,12 @@ userId = 0#temporary value, user will be assigned an ID by the server
 #main game loop
 #
 while gameState != "quit":
-    a=1#temporary to stop error
 
-    #drawBase()
-    #if gameState == 'r':#
-    #   players[userId].fillHand()
-    #   gameState = 'a'
+    #drawBase(gameState) #tells the screen drawer to draw a background appropriate for current game state
+    #function wil go live when drawing system is ready
+
+    for str in drawArr:#goes over elements in drawArr and decides which functions to call with what variables
+        todo = "yes"#tmp
 
 def newBlackCard(bigBlackDeck):#draws a new black card from the deck
     if len(bigBlackDeck) > 0:
@@ -37,4 +37,4 @@ def newBlackCard(bigBlackDeck):#draws a new black card from the deck
 
 def error(errorCode):#takes error codes and displays appropriate message
     if errorCode == "flaccidblackdeck":#error, bigBlackDeck array is empty
-        print("no cards present in black deck")
+        print("no cards present in black deck")#possible to be replaced with message on game screen
