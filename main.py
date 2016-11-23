50b1900c954ec69ab25079dd0fa94f7699e7a1b7
JonBjSig <Jaunanb@gmail.com>
2016-11-23 16:44:24 +0000

    Some minor refinements
    Added a function to player class to fill the player's hand. Refined
    gameState variable.

--- main.py
+++ main.py
@@ -3,24 +3,38 @@ import player
 import card
 import random
 
-gameState = ''#identifies current game state, m=menu, l=lobby, r=new round, q=user is asking, a=user is answering and q=quit
+gameState = "" #identifies current game state:
+#menu = user is in the menu
+#lobby = user is in the game lobby, ready to enter a game
+#newround = a new round is starting, used to prep game for new round
+#qna = a question is being asked and players are answering
+#choice = all answers have been entered and the asking player is choosing the winning entry
+#gameend = the game has ended, scores are tallied
+#quit = user has chosen to quit the game
+
+drawArr = [](str)#
+roundPos = 0#the current round position in the game, from 0 to however many players are in the game
 bigBlackDeck = [](card)#the deck holding the black cards. the deck is really big and really black
 players = [](player)#array of players in the user's game, used for drawing the game. to be populated by the server
 userId = 0#temporary value, user will be assigned an ID by the server
 
-while gameState != 'q':#main game loop
+#
+#main game loop
+#
+while gameState != "quit":
     a=1#temporary to stop error
 
     #drawBase()
-    #if gameState == 'r':
-    #   drawHand(players[userId])#
+    #if gameState == 'r':#
+    #   players[userId].fillHand()
+    #   gameState = 'a'
 
 def newBlackCard(bigBlackDeck):#draws a new black card from the deck
     if len(bigBlackDeck) > 0:
         return bigBlackDeck.pop(random.randrange(0, len(bigBlackDeck)))
     else:
-        error("flaccidblackdeck")
+        error("flaccidblackdeck")#error, bigBlackDeck array is empty
 
 def error(errorCode):#takes error codes and displays appropriate message
-    if errorCode == "flaccidblackdeck":
+    if errorCode == "flaccidblackdeck":#error, bigBlackDeck array is empty
         print("no cards present in black deck")
--- player.py
+++ player.py
@@ -1,8 +1,17 @@
 import card
+import random
+
 class player:
-    def __init__(self, playerId, gameId, playerName, playerHand, playerPos):
+    def __init__(self, playerId, gameId, playerName, playerHand, playerPos, cardPool, handSize):
         self.playerId = playerId#integer, unique identifier for each player
         self.gameId = gameId#integer, unique identifier indicating the game this player is part of
         self.playerName = playerName#string, the name the player has selected
-        self.playerHand = playerHand(card)#list holding the cards the player has in hand
+        self.playerHand = playerHand(card)#array holding the cards the player has in hand
         self.playerPos = playerPos#integer, player's position in the rotation
+        self.cardPool = cardPool(card)#array holding the cards the user can draw from, possibly temporary
+        self.handSize = handSize#maxmimum amount of cards the player can have in hand
+
+    def fillHand(self):#fills the player's hand with new cards
+        if len(self.playerHand) < self.handSize:
+            self.playerHand.append(self.cardPool.pop(random.randrange(0, len(self.cardpool))))
+            self.fillHand()
